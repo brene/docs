2df3c3b612b4696cb67510a5f8e4eeead036682e
Johannes Schickling <schickling.j@gmail.com>
2016-08-21 19:41:32 +0200

    Added overview page

--- src/pages/overview/OverviewPage.tsx
+++ src/pages/overview/OverviewPage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import {Link} from 'react-router'
 
 interface Props {
   children: Element
@@ -13,8 +14,72 @@ export default class OverviewPage extends React.Component<Props, State> {
 
   render() {
     return (
-      <div>
-        Welcome!
+      <div
+        className='flex flex-wrap pv5'
+        style={{
+          maxWidth: 800,
+          margin: '0 auto',
+        }}
+      >
+        <div className='w-100 pv4 lh-copy'>
+          This pages gives an explanation of the concepts which the Graphcool platform is based on.
+          You don't have to read the sections in any particular order, so feel free to jump around.
+        </div>
+        <div className='w-60 pr4'>
+          <h2 className='mv4'>Getting Started</h2>
+          <Link
+            to='/guides/getting-started'
+            className='relative pointer mb4 db'
+            style={{
+              height: 270,
+              background: `url(${require('../../assets/images/login.png')})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          >
+            <div className='absolute bottom-0 pa3 w-100 bg-black-30 left-0 white f6 fw6'>
+              Get started with Graphcool in 3 minutes
+            </div>
+          </Link>
+          <div>
+            <Link
+              to='/guides'
+              className='br3 ba pv3 ph4 accent f5 fw6 dib mr2 pointer'
+            >
+              View more guides
+            </Link>
+            <Link
+              to='/examples'
+              className='br3 ba pv3 ph4 accent f5 fw6 dib pointer'
+            >
+              Examples
+            </Link>
+          </div>
+        </div>
+        <div className='w-40'>
+          <h2 className='mv4'>Reference</h2>
+          <Link
+            to='/reference/platform'
+            className='br3 ba pv3 ph4 accent pointer mb3 db'
+          >
+            <div className='f4 gray-4 mb2'>Platform</div>
+            <p className='f6 gray-3 ma0'>This pages gives an explanation of the concepts which the Graphcool</p>
+          </Link>
+          <Link
+            to='/reference/simple-api'
+            className='br3 ba pv3 ph4 accent pointer mb3 db'
+          >
+            <div className='f4 gray-4 mb2'>Simple API</div>
+            <p className='f6 gray-3 ma0'>This pages gives an explanation of the concepts which the Graphcool</p>
+          </Link>
+          <Link
+            to='/reference/relay-api'
+            className='br3 ba pv3 ph4 accent pointer mb3 db'
+          >
+            <div className='f4 gray-4 mb2'>Relay API</div>
+            <p className='f6 gray-3 ma0'>This pages gives an explanation of the concepts which the Graphcool</p>
+          </Link>
+        </div>
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,9 @@ module.exports = {
       test: /\.json/,
       loader: 'json',
     }, {
+      test: /\.(jpg|png)/,
+      loader: 'file',
+    }, {
       test: /content\/.*\.svg$/,
       loader: 'file',
     }, {
